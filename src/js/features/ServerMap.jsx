1a647b83fc755f303b4a3608628992e45dc60905
Viktor Persson <viktor.persson@arcsin.se>
2022-06-08 23:18:39 +0200

    Build app.

--- src/js/features/ServerMap.jsx
+++ src/js/features/ServerMap.jsx
@@ -9,7 +9,7 @@ export const ServerMap = () => {
   const { data = [] } = useGetServersQuery({});
 
   return (
-    <MapContainer className="map" center={[0, 0]} zoom={2}>
+    <MapContainer style={{height: "100%"}} center={[0, 0]} zoom={2}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
