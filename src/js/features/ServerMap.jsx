b59d54ff19164e1b0376329dd8ff8db5f826f95d
Viktor Persson <viktor.persson@arcsin.se>
2022-06-08 23:17:31 +0200

    Update map props.

--- src/js/features/ServerMap.jsx
+++ src/js/features/ServerMap.jsx
@@ -9,12 +9,7 @@ export const ServerMap = () => {
   const { data = [] } = useGetServersQuery({});
 
   return (
-    <MapContainer
-      className="map"
-      center={[0, 0]}
-      zoom={2}
-      scrollWheelZoom={false}
-    >
+    <MapContainer className="map" center={[0, 0]} zoom={2}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -32,6 +27,6 @@ export const ServerMap = () => {
 
 const MarkerMemo = React.memo((props) => (
   <Marker position={props.coordinates}>
-    <Popup>{props.hostname}</Popup>
+    <Popup>{props.hostname.replaceAll("&#46;", ".")}</Popup>
   </Marker>
 ));
