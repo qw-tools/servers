387fc04b0b6d08e58b6cc8f033159bbc71088059
Viktor Persson <viktor.persson@arcsin.se>
2022-06-09 11:57:33 +0200

    Add link to source repository.

--- src/js/app/App.jsx
+++ src/js/app/App.jsx
@@ -24,6 +24,9 @@ export const App = () => (
             Map
           </NavLink>
         </div>
+        <div>
+          source: <a href="https://github.com/vikpe/qw-server-overview">github.com/vikpe/qw-server-overview</a>
+        </div>
       </div>
       <div className="AppBody">
         <Routes>
