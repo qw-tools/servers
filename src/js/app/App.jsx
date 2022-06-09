5cb8272ee7d4a2c73ef9e4c10fafdb651d01bf1f
Viktor Persson <viktor.persson@arcsin.se>
2022-06-09 15:06:25 +0200

    Set router basename depending on env mode.

--- src/js/app/App.jsx
+++ src/js/app/App.jsx
@@ -4,41 +4,46 @@ import { ServerMap } from "../features/ServerMap.jsx";
 import { ServerTable } from "../features/ServerTable.jsx";
 import "../../styles/index.scss";
 
-export const App = () => (
-  <BrowserRouter basename="/qw-server-overview/">
-    <div className="AppContainer">
-      <div className="AppHeader">
-        <div>
-          Display as:{" "}
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? "selected" : undefined)}
-          >
-            List
-          </NavLink>{" "}
-          |{" "}
-          <NavLink
-            to="map"
-            className={({ isActive }) => (isActive ? "selected" : undefined)}
-          >
-            Map
-          </NavLink>
+export const App = () => {
+  const basename =
+    "development" === import.meta.env.MODE ? "" : "/qw-server-overview/";
+
+  return (
+    <BrowserRouter basename={basename}>
+      <div className="AppContainer">
+        <div className="AppHeader">
+          <div>
+            Display as:{" "}
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "selected" : undefined)}
+            >
+              List
+            </NavLink>{" "}
+            |{" "}
+            <NavLink
+              to="map"
+              className={({ isActive }) => (isActive ? "selected" : undefined)}
+            >
+              Map
+            </NavLink>
+          </div>
+          <div>
+            source:{" "}
+            <a href="https://github.com/vikpe/qw-server-overview">
+              github.com/vikpe/qw-server-overview
+            </a>
+          </div>
         </div>
-        <div>
-          source:{" "}
-          <a href="https://github.com/vikpe/qw-server-overview">
-            github.com/vikpe/qw-server-overview
-          </a>
+        <div className="AppBody">
+          <Routes>
+            <Route path="/" element={<ServerTable />} />
+            <Route path="map" element={<ServerMap />} />
+          </Routes>
         </div>
       </div>
-      <div className="AppBody">
-        <Routes>
-          <Route path="/" element={<ServerTable />} />
-          <Route path="map" element={<ServerMap />} />
-        </Routes>
-      </div>
-    </div>
-  </BrowserRouter>
-);
+    </BrowserRouter>
+  );
+};
 
 export default App;
