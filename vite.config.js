45ebc9a704398038507cd649a6396bd4214aa927
Viktor Persson <viktor.persson@arcsin.se>
2023-02-17 12:16:58 +0100

    Add page entrypoints.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        details: "index.html",
+        map: "map.html",
+      },
+    },
+  },
   define: {
-    'process.env': process.env,
+    "process.env": process.env,
   },
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 9999,
-  }
 })
