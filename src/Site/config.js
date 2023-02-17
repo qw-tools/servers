3d6a2038792f96a844dc6d28fd14b7c6683cf327
Viktor Persson <viktor.persson@arcsin.se>
2023-02-17 12:49:09 +0100

    Update urls to page links.

--- src/Site/config.js
+++ src/Site/config.js
@@ -1,16 +1,16 @@
-const baseUrl = import.meta.env.DEV ? "http://localhost:5173" : import.meta.env.BASE_URL;
+const baseUrl = import.meta.env.DEV ? "http://localhost:5173/" : import.meta.env.BASE_URL;
 
 export const pages = [
   {
     title: "Details",
-    url: `${baseUrl}/`,
+    url: `${baseUrl}`,
   },
   {
     title: "World Map",
-    url: `${baseUrl}/map.html`,
+    url: `${baseUrl}map.html`,
   },
   {
     title: "Statistics",
-    url: `${baseUrl}/statistics.html`,
+    url: `${baseUrl}statistics.html`,
   },
 ];
